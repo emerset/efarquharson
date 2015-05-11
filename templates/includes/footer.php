8899cc0c5c0a3202e209d46255dc64de4ebf5b89
emerset <emerset@gmail.com>
2015-05-11 16:28:59 -0400

    include footer

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -1,1 +1,19 @@
-<?php
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><i>Website created &amp; maintained by Emerset Farquharson</i></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>

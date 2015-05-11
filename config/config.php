2592eac587486be56205459f0e0665987eb7b246
emerset <emerset@gmail.com>
2015-05-11 12:28:07 -0400

    Create config constants

--- config/config.php
+++ config/config.php
@@ -1,1 +1,9 @@
 <?php
+// DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "free4ever");
+define("DB_NAME", "talkingspace");
+
+// Paths
+define('BASE_URI', 'http://'.$_SERVER['SERVER_NAME'].'/ZendStudioWorkspace/efarquharson/');

b30d9bc331d6cd0dbb6ebca04b6cde94dd8d6865
emerset <emerset@gmail.com>
2015-05-11 12:13:19 -0400

    Customise CSS

--- index.php
+++ index.php
@@ -80,8 +80,9 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4 col-sm-4">
-        <div class="block">
-          <a href="#"><img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="140" height="140"></a>
+        <div align="center">
+        <div class="block">        
+          <a href="#"><img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="100%" height="100%"></a>
           <div class="project-text">
           <a href="#">
           <h2>Project 1</h2>
@@ -89,29 +90,32 @@
           </a>
           </div>
         </div>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4 col-sm-4">
-        <div class="block">
-        <a href="#">
-          <img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="140" height="140">
-        </a>
-          <div class="project-text">
-          <a href="#">
-          <h2>Project 2</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          </a>
-          </div>
-          
-          
         </div>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4 col-sm-4">
+        <div align="center">
         <div class="block">
-          <img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="140" height="140">
+          <a href="#">
+          <img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="100%" height="100%">
+          </a>
           <div class="project-text">
-          <h2>Project 3</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <a href="#">
+            <h2>Project 2</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            </a>
           </div>
+        </div>
+        </div>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4 col-sm-4">
+        <div align="center">
+        <div class="block">
+          <img class="img-square" src="images/project/beethoven.jpg" alt="Generic placeholder image" width="100%" height="100%">
+          <div class="project-text">
+            <h2>Project 3</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          </div>
+        </div>
         </div>
         </div><!-- /.col-lg-4 -->
         
@@ -120,30 +124,36 @@
             <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="col-lg-4 col-sm-4">
+        <div align="center">
         <div class="block">
-          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="100%" height="100%">
           <div class="project-text">
           	<h2>Project 4</h2>
           	<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
           </div>
         </div>
+        </div>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4 col-sm-4">
+        <div align="center">
         <div class="block">
-          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="100%" height="100%">
           <div class="project-text">
           	<h2>Project 5</h2>
           	<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
           </div>
         </div>
+        </div>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4 col-sm-4">
+        <div align="center">
         <div class="block">
-          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-square2" src="images/project/home6.jpg" alt="Generic placeholder image" width="100%" height="100%"">
           <div class="project-text">
           	<h2>Project 6</h2>
           	<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           </div>
+        </div>
         </div>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
